af54d9ff6d65f62e5de573b9c23c2ceceb7ac01c
Vladislav <vlad.lemeshok@gmail.com>
2025-10-06 02:21:30 +0300

    token fixed

--- Server/server.js
+++ Server/server.js
@@ -12,7 +12,10 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  credentials: true,
+  origin: "http://localhost:3000"
+}));
 app.use('/api',router);
 app.use(errorMiddleware);
 
--- Server/service/token-service.js
+++ Server/service/token-service.js
@@ -3,8 +3,8 @@ const tokenModel = require("../models/token-model");
 
 class TokenService {
     generateTokens(payload){
-        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET,{expiresIn:"30s"});
-        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET,{expiresIn:"30d"});
+        const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET,{expiresIn:"3s"});
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET,{expiresIn:"5s"});
         return {accessToken, refreshToken};
     }
 
