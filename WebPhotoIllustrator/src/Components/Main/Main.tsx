bc64cce1a94d88206625206cfbead263126ee12b
Vladislav <vlad.lemeshok@gmail.com>
2025-10-13 09:16:00 +0300

    Main Remake

--- WebPhotoIllustrator/src/Components/Main/Main.tsx
+++ WebPhotoIllustrator/src/Components/Main/Main.tsx
@@ -7,20 +7,12 @@ export default function Main() {
 
   return (
     <div className="main-container">
-      <section id="section1" className="section roboto-font d-flex">
+      <section id="section1" className="section roboto-font">
           {true ? <Auth/> : null}
       </section>
       <section id="section2" className="section">
         <h1>Section 2</h1>
         <p>Content for section 2...</p>
-      </section>
-      <section id="section3" className="section">
-        <h1>Section 3</h1>
-        <p>Content for section 3...</p>
-      </section>
-      <section id="section4" className="section">
-        <h1>Section 4</h1>
-        <p>Content for section 4...</p>
       </section>
     </div>
   );
