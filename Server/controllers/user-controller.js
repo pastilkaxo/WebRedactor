88ffbe3a7fba5807ca5b59f6a90baf09d14442ca
Vladislav <vlad.lemeshok@gmail.com>
2025-10-13 10:27:38 +0300

    fix

--- Server/controllers/user-controller.js
+++ Server/controllers/user-controller.js
@@ -11,8 +11,8 @@ class UserController {
             }
             const {email,password} = req.body;
             const userData = await userService.register(email,password);
-            res.cookie('refreshToken',userData.refreshToken,{maxAge:30*24*60*60*1000,httpOnly:true});
-            return res.json(userData);
+            res.cookie('refreshToken', userData.refreshToken, { maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true });
+           return res.json(userData);
         }
         catch(err){
            next(err);
